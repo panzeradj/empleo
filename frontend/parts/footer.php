f2cd7b045aae002ae8e8d1f1f76da34af761ba48
Pablo Mario <adnamantino@outlook.com>
2014-12-02 13:27:19 +0100

    Base de Datos
    base de datos

--- frontend/parts/footer.php
+++ frontend/parts/footer.php
@@ -3,7 +3,7 @@
 		<article id="enlaces">
 			<h2>Enlaces de Interes</h2>
 			<ul>
-				<li><a href="http://www.jcyl.es/">Junta Castilla y León</a></li>
+				<li><a href="http://www.jcyl.es/">Junta Castilla y Le&oacute;n</a></li>
 				<li><a href="http://www.datosabiertos.jcyl.es/">Datos Abiertos</a></li>
 				<li><a href="http://iesvirgendelespino.centros.educa.jcyl.es/sitio/">IES Virgen del Espino</a></li>
 				<li><a href="www.livecareer.es">Live Career</a> Curriculum Online</li>				
@@ -13,7 +13,7 @@
 		<article id="enlaces">
 			<h2>Enlaces de Interes</h2>
 			<ul>
-				<li><a href="http://www.jcyl.es/">Junta Castilla y León</a></li>
+				<li><a href="http://www.jcyl.es/">Junta Castilla y Le&oacute;n</a></li>
 				<li><a href="http://www.datosabiertos.jcyl.es/">Datos Abiertos</a></li>
 				<li><a href="http://iesvirgendelespino.centros.educa.jcyl.es/sitio/">IES Virgen del Espino</a></li>
 				<li><a href="www.livecareer.es">Live Career</a> Curriculum Online</li>				
@@ -23,7 +23,7 @@
 		<article id="enlaces">
 			<h2>Enlaces de Interes</h2>
 			<ul>
-				<li><a href="http://www.jcyl.es/">Junta Castilla y León</a></li>
+				<li><a href="http://www.jcyl.es/">Junta Castilla y Le&oacute;n</a></li>
 				<li><a href="http://www.datosabiertos.jcyl.es/">Datos Abiertos</a></li>
 				<li><a href="http://iesvirgendelespino.centros.educa.jcyl.es/sitio/">IES Virgen del Espino</a></li>
 				<li><a href="www.livecareer.es">Live Career</a> Curriculum Online</li>				
@@ -31,6 +31,6 @@
 		</article>
 	</div>	
 	<div id="copy">
-		<p>Copyright © 2014 All Rights Reserved. </p>
+		<p>Copyright &copy; 2014 All Rights Reserved. </p>
 	</div>
 </footer>
