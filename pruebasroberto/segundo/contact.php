12b0a0b88af482e003b487d36da41ed293607d20
robertopanzer <panzeradj@gmail.com>
2014-12-10 12:11:00 +0100

    tonteria
    contact coña

---
+++ pruebasroberto/segundo/contact.php
@@ -0,0 +1,33 @@
+ <?php 
+ 	include('php/functions.php'); 
+ ?> 
+
+<!DOCTYPE html>
+<html lang="es">
+	<head>		
+		<title>EmpleoJCYL.es</title>
+		<link rel="icon" href="images/favicon.png" type="image/x-icon">
+		<link rel="stylesheet" type="text/css" href="css/estilo.css">		
+		<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=true"></script>
+	</head>
+	<body>			
+		<main>
+			<!-- nav-->
+			<?php include('parts/nav.php'); ?>	
+			<div  style="  margin-left:30%; ">
+				<h2>Pablo mario</h2>
+				<a href=http://Bytelchus.com>enlace de interes(pulsar ofertas )</a><br>
+			
+					<img src="./images/pm.jpg"  style=" border: white solid; border-radius:25%; " width=100 heigth=100>
+					<br>
+					Meter cosas del curriculum
+				
+				<h2>Roberto</h2>
+				<img src="./images/r.png"  style=" border: white solid; border-radius:25%;" width=100 heigth=100><br>
+				Meter cosas del curriculum
+			</div>
+			<!-- FOOTER -->
+			<?php include('parts/footer.php');  ?>
+		</main>
+	</body>
+</html>
--- pruebasroberto/segundo/parts/nav.php
+++ pruebasroberto/segundo/parts/nav.php
@@ -5,7 +5,7 @@
 		<li><a href="index.php">inicio<span>ultimas noticias</span></a></li>
 		<li><a href="oferts.php">buscador<span>encuentra tu empleo</span></a></li>
 		<li><a href="subscribe.php">alertas<span>recibe alertas en tu email</span></a></li>
-		<li><a href="contact.php">contacto<span>averigua quienes somos</span></a></li>
+		<li><a href="#">contacto<span>averigua quienes somos</span></a></li>
 		<li><a href="#"><img src="images/twitter.png" alt="" id="twitter"></a></li>						
 	</ul>
 </nav>
